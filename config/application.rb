e87263139d4c15c17bbfbb2f6e928c10a612602f
sdmalek44 <malek.stephen44@gmail.com>
2019-10-26 13:12:40 -0600

    Change time zone to local time zone

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Chatter
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # set timezone to local time zone
+    config.time_zone = 'Mountain Time (US & Canada)'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
