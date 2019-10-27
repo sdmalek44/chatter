1e0494d5ff2cf683c62be7576280e3c07ac431c8
sdmalek44 <malek.stephen44@gmail.com>
2019-10-27 11:35:09 -0600

    Remove devise request spec helper and update messages feature spec

--- spec/features/messages/user_can_create_a_message_spec.rb
+++ spec/features/messages/user_can_create_a_message_spec.rb
@@ -3,13 +3,26 @@ require 'rails_helper'
 describe 'when logged in' do
   let(:user) { User.create(email: 'tim@tim.tim', password: 'timtim', password_confirmation: 'timtim') }
 
-  it 'can add a message' do
-    sign_in user
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:authenticate_user!).and_return(true)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
 
+  it 'can add a message' do
     visit root_path
 
     fill_in :message_body, with: 'hello'
 
     expect(page).to have_button('Send')
+  end
+
+  before do
+    5.times { user.messages.create(body: 'hello') }
+  end
+
+  it 'can see all messages' do
+    visit root_path
+
+    expect(page).to have_content('Tim: hello', count: 5)
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,4 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'devise_request_spec_helper'
 require 'spec_helper'
 
 ENV['RAILS_ENV'] ||= 'test'
@@ -47,8 +46,6 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
-  config.include DeviseRequestSpecHelper
-  config.include Devise::Test::ControllerHelpers
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
